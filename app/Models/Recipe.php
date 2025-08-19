02074de1392037a2a447d275a1245cc749de746b
Gabriel Oliveira <gabriel.a.oliveira0@hotmail.com>
2025-08-19 09:29:39 -0300

    add to model

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -19,11 +19,37 @@ class Recipe extends Model
         'metadata',
         'tags',
         'embedding',
+        // New columns added from migration
+        'recipe_code',
+        'recipe_name', 
+        'cuisine',
+        'recipe_type',
+        'service_order',
+        'preparation_time',
+        'difficulty_level',
+        'yield',
+        'channel',
+        'recipe_description',
+        'ingredients_description',
+        'preparation_method',
+        'main_ingredients',
+        'supporting_ingredients',
+        'usage_groups',
+        'preparation_techniques',
+        'consumption_occasion',
+        'general_images_link',
+        'product_code',
+        'content_code'
     ];
 
     protected $casts = [
         'metadata' => 'array',
         'tags' => 'array',
+        'main_ingredients' => 'array',
+        'supporting_ingredients' => 'array',
+        'usage_groups' => 'array',
+        'preparation_techniques' => 'array',
+        'consumption_occasion' => 'array',
     ];
 
     public function getEmbeddingVector(): ?Vector
