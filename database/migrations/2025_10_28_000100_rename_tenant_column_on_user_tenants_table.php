8c44fbc8d0db692966d2207322acc82bf224eb0d
Gabriel <gabriel.a.oliveira0@hotmail.com>
2025-10-28 12:57:35 -0300

    adjust tenant_id

--- database/migrations/2025_10_28_000100_rename_tenant_column_on_user_tenants_table.php
+++ database/migrations/2025_10_28_000100_rename_tenant_column_on_user_tenants_table.php
@@ -17,11 +17,9 @@ return new class extends Migration
                 $table->dropColumn('tenant');
             });
 
-            if (Schema::hasTable('user_tenants') && Schema::hasColumn('user_tenants', 'tenant_id')) {
-                Schema::table('user_tenants', function (Blueprint $table) {
-                    $table->foreignId('tenant_id')->constrained('tenants')->cascadeOnDelete()->cascadeOnUpdate();
-                });
-            }
+            Schema::table('user_tenants', function (Blueprint $table) {
+                $table->foreignId('tenant_id')->constrained('tenants')->cascadeOnDelete()->cascadeOnUpdate();
+            });
         }
     }
 
@@ -30,12 +28,11 @@ return new class extends Migration
      */
     public function down(): void
     {
-        if (Schema::hasTable('user_tenants') && Schema::hasColumn('user_tenants', 'tenant')) {
-            Schema::table('user_tenants', function (Blueprint $table) {
-                $table->dropForeign(['tenant_id']);
-                $table->dropColumn('tenant_id');
-            });
-        }
+        Schema::table('user_tenants', function (Blueprint $table) {
+            $table->dropForeign(['tenant_id']);
+            $table->dropColumn('tenant_id');
+            $table->string('tenant')->nullable();
+        });
     }
 };
 
