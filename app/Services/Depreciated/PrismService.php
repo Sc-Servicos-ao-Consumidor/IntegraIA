4995e98d563f064e2c364c5769f528a5dc0cb8be
Leonardo Augustus <leoaugusto45@gmail.com>
2026-05-26 09:37:55 -0300

    correcoes e remoção de pacote Prism

--- app/Services/Depreciated/PrismService.php
+++ app/Services/Depreciated/PrismService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Depreciated;
 
 use Illuminate\Support\Facades\Log;
 use Prism\Prism\Enums\Provider;
--- app/Services/Depreciated/ProductImportService.php
+++ app/Services/Depreciated/ProductImportService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Depreciated;
 
 use App\Models\GroupProduct;
 use App\Models\Product;
